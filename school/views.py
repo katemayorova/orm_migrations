1e7a56a4220423c52f07a9716dca3f4376f02ecc
katemayorova <katemayorova0@gmail.com>
2022-07-28 22:45:23 +0300

    Optimize sql queries by using prefetch_related

--- school/views.py
+++ school/views.py
@@ -7,7 +7,7 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     context = {
-        'object_list': Student.objects.all()
+        'object_list': Student.objects.all().prefetch_related('teachers')
     }
 
     # используйте этот параметр для упорядочивания результатов
